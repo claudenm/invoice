6ec4e9b44c19325d66ba17de7d486484661c5649
claudenm <claude.mendelson@newscred.com>
2014-03-13 23:21:05 -0400

    added updated for lineitems

--- invoice.py
+++ invoice.py
@@ -15,7 +15,7 @@ class Invoice(db.Model):
     date_added = db.DateTimeProperty(auto_now_add=True)
     sent = db.BooleanProperty()
     void = db.BooleanProperty()
-    
+
 
 class LineItem(db.Model):
     units_billed=db.FloatProperty()
@@ -23,25 +23,25 @@ class LineItem(db.Model):
     rate = db.FloatProperty()
     date_worked = db.DateTimeProperty()
     invoice = db.ReferenceProperty(Invoice,collection_name='line_items' )
-    
+
 class MainPage(webapp.RequestHandler):
     def get(self):
         invoices_query = Invoice.all().order('-date_added')
         invoices = invoices_query.fetch(10)
-        
+
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
-        
+
         template_values = {
-            'invoices': invoices,
-            'url': url,
-            'url_linktext': url_linktext,
+        'invoices': invoices,
+        'url': url,
+        'url_linktext': url_linktext,
         }
-        
+
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
@@ -50,20 +50,20 @@ class Invoices(webapp.RequestHandler):
     def get(self):
         invoices_query = Invoice.all().order('-date')
         invoices = invoice_query.fetch(10)
-        
-        
+
+
         template_values = {
-                'invoices': invoices
+        'invoices': invoices
         }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
 #Create Invoice
 class AddInvoice(webapp.RequestHandler):
-    
+
     def get(self):
         template_values = {
-            'units' : LineItem.unit.choices
+        'units' : LineItem.unit.choices
         }
 
         path = os.path.join(os.path.dirname(__file__), 'invoices.html')
@@ -73,7 +73,7 @@ class AddInvoice(webapp.RequestHandler):
         invoice = Invoice()
         invoice.description = self.request.get('description')
         invoice.put()
-        
+
         lineitem = LineItem()
         logging.error( 'WTF - ' + self.request.get('rate'))
         lineitem.units_billed = float(self.request.get('units_billed'))
@@ -86,47 +86,50 @@ class AddInvoice(webapp.RequestHandler):
 
 #Edit Invoice
 class EditInvoice(webapp.RequestHandler):
-    def get(self):
-		invoice=Invoice.get_by_id(int(self.request.GET['id']))
-    		line_items = invoice.line_items.fetch(5)
-    		for li in line_items:
-    			logging.error(li.rate)
-        	template_values = {
-	    	'invoice' : invoice,
-            'units' : LineItem.unit.choices,
-            'line_items' : line_items
-        }
-        	path = os.path.join(os.path.dirname(__file__), 'edit_invoice.html')
-        	self.response.out.write(template.render(path, template_values))
-    
+    pass
+
 
 #Edit Line Item
 class EditItem(webapp.RequestHandler):
+    def get(self):
+        invoice=Invoice.get_by_id(int(self.request.GET['id']))
+        line_items = invoice.line_items.fetch(5)
+        for li in line_items:
+            logging.error(li.rate)
+        template_values = {
+        'invoice' : invoice,
+        'units' : LineItem.unit.choices,
+        'line_items' : line_items
+            }
+        path = os.path.join(os.path.dirname(__file__), 'edit_invoice.html')
+        self.response.out.write(template.render(path, template_values))
 
-	def post(self):
-		lineitem = LineItem.get_by_id(self.request.id)
-		lineitem.units_billed = float(self.request.get('units_billed'))
-		lineitem.rate = self.request.get('rate')
-		lineitem.units = self.request.get('units')
-		lineitem.date_worked = datetime.strptime(self.request.get('date'), '%d/%m/%Y')
-		lineitem.invoice = invoice
-		lineitem.put()
-		self.request.redirect('/')
+    def post(self):
+        logging.error(self)
+        lineitem = LineItem.get_by_id(int(self.request.POST['id']))
+        lineitem.units_billed = float(self.request.get('units_billed'))
+        lineitem.rate = float(self.request.get('rate'))
+        lineitem.units = self.request.get('units')
+        lineitem.date_worked = datetime.strptime(self.request.get('date'), '%d/%m/%Y')
+        lineitem.put()
+        self.redirect('/')
+
 
 
 application = webapp.WSGIApplication([('/', MainPage),
-                                      ('/invoices', MainPage),
-                                      ('/invoice/add', AddInvoice),
-                                      ('/invoice/edit', EditInvoice),
-				      				  ('/item/edit', EditItem)],
-                                     debug=True)
+                          ('/invoices', MainPage),
+                          ('/invoice/add', AddInvoice),
+                          ('/item/update', EditItem),
+                          ('/item/edit', EditItem)
+	      				  ],
+                         debug=True)
 
 '''application = webapp.WSGIApplication(
-                                     [('/', MainPage),
-                                      ('/invoices', MainPage),
-                                      ('/invoice/add', AddInvoice),
-                                      (r'invoice/<id:(\d+)>', EditInvoice)],
-                                     debug=True)'''
+[('/', MainPage),
+('/invoices', MainPage),
+('/invoice/add', AddInvoice),
+(r'invoice/<id:(\d+)>', EditInvoice)],
+debug=True)'''
 
 
 def main():
